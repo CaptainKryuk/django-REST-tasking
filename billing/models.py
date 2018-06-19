0df52aff0b9719c2cb204baa0cdc04a301df6b75
CaptainKryuk <bestrongwb@gmail.com>
2018-06-19 16:28:02 +0300

    Start billing

--- billing/models.py
+++ billing/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from base.models import AbstractDataTimeModel
 
-# Create your models here.
+
+class MoneyLog(AbstractDataTimeModel):
+    """
+    Record all actions with money
+    """
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    reason = models.CharField(max_length=255)
+    debit = models.DecimalField(decimal_places=2, max_digits=7, default=0)
+    credit = models.DecimalField(decimal_places=2, max_digits=7, default=0)
+    money = models.DecimalField(decimal_places=2, max_digits=7, default=0)
+
+class TaskExpense(AbstractDataTimeModel):
+    """
+    Record all task actions
+    """
+    task = models.ForeignKey('task.Task', on_delete=models.CASCADE)
+    executor = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    money = models.DecimalField(max_digits=7, decimal_places=2)
--- tasking/users/models.py
+++ tasking/users/models.py
@@ -17,9 +17,14 @@ class User(AbstractUser):
     # around the globe.
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
     user_type = models.PositiveSmallIntegerField(choices=USER_TYPES, default=executor)
+    balance = models.DecimalField(decimal_places=2, max_digits=7)
+
 
     def __str__(self):
         return self.username
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
+
+    def update_balance(self, reason, **kwargs):
+        pass
